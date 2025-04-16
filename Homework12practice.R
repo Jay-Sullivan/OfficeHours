935cc6bbc1c65322d4c2bab6abafde33980f416b
Jay-Sullivan <jayivmail@gmail.com>
2025-04-16 14:02:57 -0400

    Corrected homework 10

--- Homework12practice.R
+++ Homework12practice.R
@@ -27,10 +27,13 @@ outer_space_objects <- tuesdata$outer_space_objects
 # 10 countries, be able to read
 filtered_data <- outer_space_objects %>%
   filter(Entity %in% c("Canada", "Brazil", "China", "Denmark","Argentina","Chile","Colombia","Finland","France","Germany"))
+colnames(filtered_data) <- c("Country","Code","Year","Objects")
+# First correct data
 
+# Ridgeline
 p2 <- ggplot(data=filtered_data) +
-  aes(x=Year,y=Entity,fill=num_objects) +
-  ggridges::geom_density_ridges(color="black",fill="lightblue") +
+  aes(x=Year,y=Country,fill=Country) +
+  ggridges::geom_density_ridges() +
   ggridges::theme_ridges(font_size = 14,
                          font_family = "Times New Roman",
                          line_size = 0.5,
@@ -41,9 +44,11 @@ p2
 # Beeswarm plot
 filtered_data2 <- outer_space_objects %>%
   filter(Entity %in% c("Canada", "Brazil", "China"))
+colnames(filtered_data2) <- c("Country","Code","Year","Objects")
+# Second Correct Data
 
 p3 <- ggplot(data=filtered_data2) +
-  aes(x=Entity,y=num_objects,color=Entity) +
+  aes(x=Country,y=Objects,color=Country) +
   ggbeeswarm::geom_beeswarm(method = "center",size=1.5) +
   scale_color_manual(values = c(
     "Canada" = "cornflowerblue",
@@ -59,18 +64,22 @@ p3
 
 # typical unsatisfactory pie chart
 p7 <- ggpie::ggpie(data=filtered_data2,
-                   group_key="Entity",
+                   group_key="Country",
                    count_type="full",
                    label_info="ratio",
-                   label_type="circle") # try circle
+                   label_type="circle") + # try circle
+  ggtitle("Number of Satellites sent to Space divided by Country")
+
 p7
 
 # much better waffle plot
-filtered_data2 <- outer_space_objects %>%
+filtered_data3 <- outer_space_objects %>%
   filter(Entity %in% c("Canada", "Brazil", "Germany","Denmark"))
+colnames(filtered_data3) <- c("Country","Code","Year","Objects")
 
-p8 <- ggplot(data=filtered_data2) +
-  aes(fill = Entity, values = num_objects) +
+p8 <- ggplot(data=filtered_data3) +
+  aes(fill = Country, values = Objects) +
+  ggtitle("    Number of individuals Satellites Sent to Space") +
   waffle::geom_waffle(n_rows = 8, size = 0.33, colour = "white") +
   coord_equal() +
   theme_void()
